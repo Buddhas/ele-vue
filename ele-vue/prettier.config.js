91357b6b48e34fed19a851fe6fd21f8200549694
chenbin <2424932515@qq.com>
2019-10-25 15:21:59 +0800

    添加eslint配置文件

---
+++ ele-vue/prettier.config.js
@@ -0,0 +1,14 @@
+// prettier.config.js or .prettierrc.js
+module.exports = {
+  trailingComma: 'none', // 如果使用 all 选项需要 node 8
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  jsxBracketSameLine: true,
+  arrowParens: 'avoid',
+  endOfLine: 'lf',
+  bracketSpacing: true,
+  htmlWhitespaceSensitivity: 'strict'
+}
