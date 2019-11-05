b6dc79af253a7ab74f274b0dfa712a84156ea558
chenbin <2424932515@qq.com>
2019-11-05 22:28:16 +0800

    商家列表头部开发

---
+++ ele-vue/src/components/storeListHead.vue
@@ -0,0 +1,75 @@
+<template>
+  <section class="header">
+    <div class="filter-header">
+      <a class="filter-nav" @click="showSortFlag=true">综合排序</a>
+      <a class="filter-nav">距离最近</a>
+      <a class="filter-nav">品质联盟</a>
+      <a class="filter-nav">筛选</a>
+    </div>
+    <section class="sort">
+      <ul class="item-list">
+        <li v-for="(item, index) in sortItems" :key="index" class="item">{{ item }}</li>
+      </ul>
+    </section>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showSortFlag: false,
+      sortItems: [
+        '综合排序',
+        '好评优先',
+        '好评优先',
+        '好评优先',
+        '好评优先',
+        '好评优先',
+        '好评优先',
+        '好评优先'
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.header {
+  position: relative;
+}
+.filter-header {
+  display: flex;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  width: 100%;
+  z-index: 4;
+  height: 40px;
+  line-height: 40px;
+  background-color: #ffffff;
+  .filter-nav {
+    flex: 1;
+    text-align: center;
+    color: #666;
+    position: relative;
+    font-size: 0.373333rem;
+    z-index: 101;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+}
+.sort {
+  width: 100%;
+  background-color: #ffffff;
+  position: absolute;
+  top: 40px;
+  .item {
+    font-size: 14px;
+    color: black;
+    padding-left: 20px;
+    line-height: 40px;
+  }
+}
+</style>
--- ele-vue/src/views/main/index.vue
+++ ele-vue/src/views/main/index.vue
@@ -2,17 +2,53 @@
   <div class="main-wrapper">
     <Head />
     <ScrollFoodCategory />
+    <div class="store-list">
+      <p class="title flex align_center justify_center">推荐商家</p>
+      <StoreListHead />
+    </div>
   </div>
 </template>
 
 <script>
 import Head from './components/head'
 import ScrollFoodCategory from './components/scrollFoodCategory'
-
+import StoreListHead from 'components/storeListHead'
 export default {
   components: {
     Head,
-    ScrollFoodCategory
+    ScrollFoodCategory,
+    StoreListHead
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  .store-list {
+    background-color: #fff;
+    .title {
+      font-size: 16px;
+      color: black;
+      line-height: 40px;
+      padding-top: 20px;
+      padding-bottom: 20px;
+      text-align: center;
+      padding: 0 5px;
+      &::before {
+        display: block;
+        margin-right: .135135rem;
+        content: "";
+        width: .533333rem;
+        height: .026667rem;
+        background-color:#999;
+      }
+      &::after {
+        display: block;
+        margin-left: .135135rem;
+        content: "";
+        width: .533333rem;
+        height: .026667rem;
+        background-color:#999;
+      }
+    }
+  }
+</style>
