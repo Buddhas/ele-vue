d1f9682a9ea65ae6bb716437fba7476d3fd82b6d
chenbin <2424932515@qq.com>
2019-12-31 18:30:13 +0800

    商家详情组件开发

--- ele-vue/src/views/shopDetail/components/ratings.vue
+++ ele-vue/src/views/shopDetail/components/ratings.vue
@@ -75,4 +75,200 @@
   </cube-scroll>
 </template>
 
+<script>
+import Star from './star'
+import RatingSelect from './ratingSelect'
+import ratingMixin from '@/common/mixins/rating'
+import moment from 'moment'
 
+export default {
+  name: 'Ratings',
+  mixins: [ratingMixin],
+  components: {
+    Star,
+    RatingSelect
+  },
+  props: {
+    data: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  data() {
+    return {
+      ratings: [],
+      scrollOptions: {
+        click: false,
+        directionLockThreshold: 0
+      }
+    }
+  },
+  computed: {
+    seller() {
+      return this.data.seller || {}
+    }
+  },
+  methods: {
+    fetch() {
+      if (!this.fetched) {
+        this.fetched = true
+      }
+    },
+    format(time) {
+      return moment(time).format('YYYY-MM-DD hh:mm')
+    }
+  },
+  watch: {
+    selectType() {
+      this.$nextTick(() => {
+        this.$refs.scroll.refresh()
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .ratings {
+    position: relative;
+    text-align: left;
+    white-space: normal;
+    height: 100%;
+    .overview {
+      display: flex;
+      padding: 18px 0;
+      .overview-left {
+        flex: 0 0 137px;
+        padding: 6px 0;
+        width: 137px;
+        border-right: 1px solid #d9dde1;
+        text-align: center;
+        .score {
+          margin-bottom: 6px;
+          line-height: 28px;
+          font-size: 24px;
+          color: #fc9153;
+        }
+        .title {
+          margin-bottom: 8px;
+          line-height: 12px;
+          font-size: 12px;
+          color: #333;
+        }
+        .rank {
+          line-height: 10px;
+          font-size: 12px-s;
+          color: #999;
+        }
+      }
+      .overview-right {
+        flex: 1;
+        padding: 6px 0 6px 24px;
+        .score-wrapper {
+          display: flex;
+          align-items: center;
+          margin-bottom: 8px;
+          .title {
+            line-height: 18px;
+            font-size: 12px;
+            color: #333;
+          }
+          .star {
+            margin: 0 12px;
+          }
+          .score {
+            line-height: 18px;
+            font-size: 12px;
+            color: #fc9153;
+          }
+        }
+        .delivery-wrapper {
+          display: flex;
+          align-items: center;
+          .title {
+            line-height: 18px;
+            font-size: 12px;
+            color: #333;
+          }
+          .delivery {
+            margin-left: 12px;
+            font-size: 12px;
+            color: #999;
+          }
+        }
+      }
+    }
+    .rating-wrapper {
+      padding: 0 18px;
+      .rating-item {
+        display: flex;
+        padding: 18px 0;
+        .avatar {
+          flex: 0 0 28px;
+          width: 28px;
+          margin-right: 12px;
+          img {
+            height: auto;
+            border-radius: 50%;
+          }
+        }
+        .content {
+          position: relative;
+          flex: 1;
+          .name {
+            margin-bottom: 4px;
+            line-height: 12px;
+            font-size: 12px-s;
+            color: #333;
+          }
+          .star-wrapper {
+            margin-bottom: 6px;
+            display: flex;
+            align-items: center;
+            .star {
+              margin-right: 6px
+            }
+            .delivery {
+              font-size: 12px-s;
+              color: #999;
+            }
+          }
+          .text {
+            margin-bottom: 8px;
+            line-height: 18px;
+            color: #333;
+            font-size: 12px;
+          }
+          .recommend {
+            display: flex;
+            align-items: center;
+            flex-wrap: wrap;
+            line-height: 16px;
+            .icon-thumb_up, .item {
+              margin: 0 8px 4px 0;
+              font-size: 12px;
+            }
+            .icon-thumb_up {
+              color: #00a0dc;
+            }
+            .item {
+              padding: 0 6px;
+              border: 1px solid rgba(7,17,27,.1);
+              border-radius: 1px;
+              color: #999;
+              background: #fff;
+            }
+          }
+          .time {
+            position: absolute;
+            top: 0;
+            right: 0;
+            line-height: 12px;
+            font-size: 12px;
+            color: #999;
+          }
+        }
+      }
+    }
+  }
+</style>
--- ele-vue/src/views/shopDetail/components/seller.vue
+++ ele-vue/src/views/shopDetail/components/seller.vue
@@ -1,5 +1,280 @@
 <template>
-  <div class="goods-wrapper">
-    seller
-  </div>
+  <cube-scroll class="seller" :options="sellerScrollOptions">
+    <div class="seller-content">
+      <div class="overview">
+        <h1 class="title">{{ seller.name }}</h1>
+        <div class="desc border-bottom-1px">
+          <star :size="36" :score="seller.score" />
+          <span class="text">({{ seller.ratingCount }})</span>
+          <span class="text">月售{{ seller.sellCount }}单</span>
+        </div>
+        <ul class="remark">
+          <li class="block">
+            <h2>起送</h2>
+            <div class="content">
+              <span class="stress">{{ seller.minPrice }}</span>元
+            </div>
+          </li>
+          <li class="block">
+            <h2>商家配送</h2>
+            <div class="content">
+              <span class="stress">{{ seller.deliveryPrice }}</span>元
+            </div>
+          </li>
+          <li class="block">
+            <h2>平均配送时间</h2>
+            <div class="content">
+              <span class="stress">{{ seller.deliveryTime }}</span>分钟
+            </div>
+          </li>
+        </ul>
+        <div class="favorite" @click="toggleFavorite">
+          <span class="icon-favorite" :class="{ active: favorite }"></span>
+          <span class="text">{{ favoriteText }}</span>
+        </div>
+      </div>
+      <div class="bulletin">
+        <h1 class="title">公告与活动</h1>
+        <div class="content-wrapper border-bottom-1px">
+          <p class="content">{{ seller.bulletin }}</p>
+        </div>
+        <ul v-if="seller.supports" class="supports">
+          <li
+            v-for="(item, index) in seller.supports"
+            :key="index"
+            class="support-item border-bottom-1px"
+          >
+            <support-ico
+              :size="4"
+              :type="seller.supports[index].type"
+            />
+            <span class="text">{{ seller.supports[index].description }}</span>
+          </li>
+        </ul>
+      </div>
+      <div class="pics">
+        <h1 class="title">商家实景</h1>
+        <cube-scroll class="pic-wrapper" :options="picScrollOptions">
+          <ul class="pic-list">
+            <li
+              v-for="(pic, index) in seller.pics"
+              :key="index"
+              class="pic-item"
+            >
+              <img :src="pic" width="120" height="90" />
+            </li>
+          </ul>
+        </cube-scroll>
+      </div>
+      <div class="info">
+        <h1 class="title border-bottom-1px">商家信息</h1>
+        <ul>
+          <li
+            v-for="(info, index) in seller.infos"
+            :key="index"
+            class="info-item border-bottom-1px"
+          >
+            {{ info }}
+          </li>
+        </ul>
+      </div>
+    </div>
+  </cube-scroll>
 </template>
+
+<script>
+import Star from './star'
+import SupportIco from './supportIco'
+export default {
+  components: {
+    SupportIco,
+    Star
+  },
+  props: {
+    data: {
+      type: Object,
+      default() {
+        return {}
+      }
+    }
+  },
+  data() {
+    return {
+      favorite: false,
+      sellerScrollOptions: {
+        directionLockThreshold: 0,
+        click: false
+      },
+      picScrollOptions: {
+        scrollX: true,
+        stopPropagation: true,
+        directionLockThreshold: 0
+      }
+    }
+  },
+  computed: {
+    seller() {
+      return this.data.seller || {}
+    },
+    favoriteText() {
+      return this.favorite ? '已收藏' : '收藏'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .seller {
+    height: 100%;
+    text-align: left;
+    .overview {
+      position: relative;
+      padding: 18px;
+      .title {
+        margin-bottom: 8px;
+        line-height: 14px;
+        font-size: 14px;
+        color: #333;
+      }
+      .desc {
+        display: flex;
+        align-items: center;
+        padding-bottom: 18px;
+        .star {
+          margin-right: 8px
+        }
+        .text {
+          margin-right: 12px;
+          line-height: 18px;
+          font-size: 10px;
+          color: #666;
+        }
+      }
+      .remark {
+        display: flex;
+        padding-top: 18px;
+        .block {
+          flex: 1;
+          text-align: center;
+          border-right: 1px solid #d9dde1;
+          &:last-child {
+            border: none
+          }
+          h2 {
+            margin-bottom: 4px;
+            line-height: 10px;
+            font-size: 10px;
+            color: #999;
+          }
+          .content {
+            line-height: 24px;
+            font-size: 10px;
+            color: #333;
+            .stress {
+              font-size: 24px;
+            }
+          }
+        }
+      }
+      .favorite {
+        position: absolute;
+        width: 50px;
+        right: 11px;
+        top: 18px;
+        text-align: center;
+        .icon-favorite {
+          display: block;
+          margin-bottom: 4px;
+          line-height: 24px;
+          font-size: 24px;
+          color: #d9dde1;
+          &.active {
+            color: #f01414;
+          }
+        }
+        .text {
+          line-height: 10px;
+          font-size: 10px;
+          color: #666;
+        }
+      }
+    }
+    .bulletin {
+      padding: 18px 18px 0 18px;
+      white-space: normal;
+      .title {
+        margin-bottom: 8px;
+        line-height: 14px;
+        color: #333;
+        font-size: 14px;
+      }
+      .content-wrapper {
+        padding: 0 12px 16px 12px;
+        .content {
+          line-height: 24px;
+          font-size: 12px;
+          color: #f01414;
+        }
+      }
+      .supports {
+        .support-item {
+          display: flex;
+          align-items: center;
+          padding: 16px 12px;
+          &:last-child {
+            border: 0px;
+          }
+        }
+        .support-ico {
+          margin-right: 6px;
+        }
+        .text {
+          line-height: 16px;
+          font-size: 12px;
+          color: #333;
+        }
+      }
+    }
+    .pics {
+      padding: 18px;
+      .title {
+        margin-bottom: 12px;
+        line-height: 14px;
+        color: #333;
+        font-size: 14px;
+      }
+      .pic-wrapper {
+        display: flex;
+        align-items: center;
+        .pic-list {
+          .pic-item {
+            display: inline-block;
+            margin-right: 6px;
+            width: 120px;
+            height: 90px;
+            &:last-child {
+               margin: 0
+            }
+          }
+        }
+      }
+    }
+    .info {
+      padding: 18px 18px 0 18px;
+      color: #333;
+      .title {
+        padding-bottom: 12px;
+        line-height: 14px;
+        font-size: 14px;
+      }
+      .info-item {
+        padding: 16px 12px;
+        line-height: 16px;
+        font-size: 12px;
+        &:last-child {
+          border: 0px;
+        }
+      }
+    }
+  }
+</style>
