fd8eb471dc71d21f092afbbce9d930d412a0890f
chenbin <2424932515@qq.com>
2020-01-07 21:06:25 +0800

    feat：地址组件开发

---
+++ ele-vue/src/views/address/components/item.vue
@@ -0,0 +1,64 @@
+<template>
+  <section class="address-item">
+    <div class="user-info">
+      <span style="color: #333;font-weight: 700;margin-right:5px">陈斌</span><span style="margin-right:5px">先生</span><span>17688702092</span>
+    </div>
+    <div class="flex bottom">
+      <div class="message flex align_center">
+        <span class="home">家</span>
+        <span class="detail">德力西大厦十二楼德力西大厦十二楼德力西大</span>
+      </div>
+      <div class="icon-wrapper">
+        <span>1</span>
+        <span>2</span>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+  .address-item {
+    padding: 15px 10px;
+    background-color: #fff;
+    border-bottom: 1px solid #ddd;
+    .user-info {
+      font-size: 18px;
+      color: #666;
+    }
+    .bottom {
+      margin-top: 8px;
+      .message {
+        flex: 1;
+        .detail {
+          margin-left: 3px;
+          font-size: 16px;
+          color: #666;
+        }
+        .home {
+          color: #ff5722;
+          border: 1px solid #ff5722;
+          font-size: 10px;
+          display: inline-block;
+          padding: 1px 2px;
+        }
+        .company  {
+          color: #3190e8;
+          border: 1px solid #3190e8;
+          font-size: 10px;
+          display: inline-block;
+          padding: 1px 2px;
+        }
+        .school {
+          color: #00d762;
+          border: 1px solid #00d762;
+          font-size: 10px;
+          display: inline-block;
+          padding: 1px 2px;
+        }
+      }
+      .icon-wrapper {
+        width: 40px;
+      }
+    }
+  }
+</style>
--- ele-vue/src/views/address/index.vue
+++ ele-vue/src/views/address/index.vue
@@ -1,9 +1,41 @@
 <template>
   <div class="address-index-wrapper">
-    
+    <div class="item-wrapper">
+      <Item v-for="(item, index) in 10" :key="index" />
+    </div>
+    <div class="bottom">
+      新增地址
+    </div>
   </div>
 </template>
 
+<script>
+import Item from './components/item'
+export default {
+  components: {
+    Item
+  }
+}
+</script>
 <style lang="scss" scoped>
-  
+  .address-index-wrapper {
+    width: 100%;
+    background-color: #fff;
+    .item-wrapper {
+      padding-bottom: 60px;
+    }
+    .bottom {
+      width: 100%;
+      z-index: 10;
+      position: fixed;
+      height: 50px;
+      border-top: 1px solid #ddd;
+      line-height: 50px;
+      background: #fff;
+      bottom: 0px;
+      text-align: center;
+      font-size: 18px;
+      color: #3190e8;
+    }
+  }
 </style>
