03a5f552f75bd3469fa17ed56bcefc364f3dca08
e <2424932515@qq.com>
2020-03-22 17:17:00 +0800

    feat:修改镜像名称

--- ele-vue/setup.sh
+++ ele-vue/setup.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 image_version=`date +%Y%m%d%H%M`;
-# 关闭shop_admin容器
-docker stop shop_admin || true;
-# 删除shop_admin容器
-docker rm shop_admin || true;
-# 删除shop/admin镜像
-docker rmi --force $(docker images | grep shop/admin | awk '{print $3}')
-# 构建shop/admin:$image_version镜像
-docker build . -t shop/admin:$image_version;
+# 关闭ele_index_vue容器
+docker stop ele_index_vue || true;
+# 删除ele_index_vue容器
+docker rm ele_index_vue || true;
+# 删除ele/index/vue镜像
+docker rmi --force $(docker images | grep ele/index/vue | awk '{print $3}')
+# 构建ele/index/vue:$image_version镜像
+docker build . -t ele/index/vue:$image_version;
 # 查看镜像列表
 docker images;
-# 基于shop/admin 镜像 构建一个容器 shop_admin
-docker run -p 9529:8080 -d --name shop_admin shop/admin:$image_version;
+# 基于ele/index/vue 镜像 构建一个容器 ele_index_vue
+docker run -p 9529:8080 -d --name ele_index_vue ele/index/vue:$image_version;
 # 查看日志
-docker logs shop_admin;
+docker logs ele_index_vue;
 #删除build过程中产生的镜像    #docker image prune -a -f
 docker rmi $(docker images -f "dangling=true" -q)
 # 对空间进行自动清理
